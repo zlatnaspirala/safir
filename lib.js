5bbfbda1bad887df518f98bba640b603a886c73a
zlatnaspirala <zlatnaspirala@gmail.com>
2022-10-10 22:23:37 +0200

    build.lib Build only lib

---
+++ lib.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ * @description
+ * Main export file for Safir.
+ */
+
+import {
+  getComp,
+  T,
+  Safir } from "./src/core/root";
+import { BaseComponent } from "./src/core/comp";
+import { On } from "./src/core/modifier";
+
+console.info("Safir loaded.", Safir);
